f37f41987694e4863094a656d2cc3a13280cfe69
nasir456244 <arturlegendflame@gmail.com>
2022-10-10 01:09:31 +1000

    added link to navbar

--- components/EmptyTable.jsx
+++ components/EmptyTable.jsx
@@ -15,22 +15,22 @@ const EmptyTable = () => {
                                 <th 
                                     className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left 
                                     text-xs font-semibold text-gray-700 uppercase tracking-wider">
-                                    Client / Invoice
+                                    title
                                 </th>
                                 <th
                                     className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left 
                                     text-xs font-semibold text-gray-700 uppercase tracking-wider">
-                                    Amount
+                                    description
                                 </th>
                                 <th
                                     className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 
                                     text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
-                                    Issued / Due
+                                    date
                                 </th>
                                 <th
                                     className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left 
                                     text-xs font-semibold text-gray-700 uppercase tracking-wider">
-                                    Status
+                                    status
                                 </th>
                                 <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100"></th>
                             </tr>
