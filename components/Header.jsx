6e81ea2acc59e62ec954e550a26a2f140becf2c6
nasir456244 <arturlegendflame@gmail.com>
2022-10-11 23:24:47 +1000

    added icon

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, useContext } from "react";
 import Link from "next/link";
 import { CollectBrisbaneContext } from "@/context/CollectBrisbane";
-import {icon} from '@/public/icon.png'
+import icon from '@/public/icon.png'
 import Image from "next/image";
 const Header = () => {
   const [openNav, setOpenNav] = useState(false);
@@ -55,7 +55,7 @@ const Header = () => {
         <h5
           className="text-[#000] gap-2 flex items-center justify-center tracking-normal font-sans text-2xl leading-snug mr-4 cursor-pointer py-1.5 font-normal"
         >
-          <Image src={icon} width={40} height={35} alt="icon" />
+          <Image src="../public/icon.png" width={40} height={35} alt="icon" />
           <span className="sm:hidden md:flex">AUC</span>
         </h5>
         </Link>
